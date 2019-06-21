7ae23e5ec3c77390e4be4bee192a19ec92735745
AndrewMPFQ <andrewf9001@gmail.com>
2019-06-21 13:17:21 -0600

    heroku setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAsgqvZNtLe5ZzYbbp0IOZCxiJF-5uZnUg",
+  authDomain: "urban-vapor.firebaseapp.com",
+  databaseURL: "https://urban-vapor.firebaseio.com",
+  projectId: "urban-vapor",
+  storageBucket: "urban-vapor.appspot.com",
+  messagingSenderId: "643602597949",
+  appId: "1:643602597949:web:7339cfc90b20146d"
+};
+
+export default firebaseConfig;
