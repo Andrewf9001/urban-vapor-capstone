048336299d8f7b817796a60dba7175e3c08283b1
AndrewMPFQ <andrewf9001@gmail.com>
2019-11-17 11:33:50 -0700

    updated auth page, new images

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 import Login from "../auth/login";
+import Logo from "../../../static/assets/images/store-images/counter-backdrop.png";
 import Footer from "../navigation/footer";
 
 export default function() {
   return (
     <div className="auth-wrapper">
       <div className="auth-page-wrapper">
-        <div className="auth-left-column" />
-
+        <div className="auth-left-column">
+          <img src={Logo} alt="Logo" />
+        </div>
         <div className="auth-right-column">
           <Login />
         </div>
