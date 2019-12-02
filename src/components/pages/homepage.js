ccf64cf735a018dc2a65be06632bf1eabbd7ca8f
AndrewMPFQ <andrewf9001@gmail.com>
2019-12-02 14:48:42 -0700

    added in photo version of homepage

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 import Footer from "../navigation/footer";
 import BrandLogo from "../../../static/assets/images/c4lnowarning.png";
-// import Logo from "../../../static/assets/images/clouds4less-menu.png";
-// import Logo2 from "../../../static/assets/images/clouds4less-menu.png";
+import Logo from "../../../static/assets/images/clouds4less-menu.png";
+import Logo2 from "../../../static/assets/images/clouds4less-menu.png";
 
 export default class Homepage extends Component {
   constructor() {
@@ -29,6 +29,8 @@ export default class Homepage extends Component {
           <img src={BrandLogo} alt="Logo" />
         </div>
         <div className="cloud-menus">
+          <img src={Logo} alt="Logo" />
+          <img src={Logo2} alt="Logo2" />
           {/* <ul>
             {this.state.cloud_items.map(cloud_items => (
               <li>{cloud_items.flavors}</li>
